cd55520a3017bde1ce4db344f9a9679cfad9184d
Hixsav <hi.savis@gmail.com>
2018-06-05 14:50:33 +0000

    Add appointments event

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,10 @@
 class AppointmentsController < ApplicationController
   def index
+    # define appointments method to GET appointments (or events) and display in ascending (ASC) time order
+    @appointments = Appointment.order('apt_time ASC')
+    # define appointment as a new record
+    @appointment = Appointment.new
   end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'appointments#index'
+  resources :appointments
 end
 
